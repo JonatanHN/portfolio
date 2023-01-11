498f917528b2548da71bb613800a20d546ca1cc5
JonatanHN <jonapato00@gmail.com>
2023-01-10 21:14:23 -0600

    New portfolio

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { Link } from "gatsby";
+
+const AboutPage = () => {
+  return (
+    <main>
+      <title>Hi about me</title>
+      <Link to="/">
+        Home
+      </Link>
+    </main>
+  )
+} 
+
+export default AboutPage;
